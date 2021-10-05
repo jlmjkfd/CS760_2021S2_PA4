796fa0bb1721462a109f454dc22015c63942bbe9
Irabella Lin <irabella.lin0718@gmail.com>
2021-10-05 13:14:59 +1300

    added folder presentation 4 and improved dataset

--- 4_DTM/num_tweets_per_slice.py
+++
@@ -1,6 +0,0 @@
-PA4_data_nobrand=pd.read_csv("PA4DATA_nobrand.csv",sep="\t")
-open_date='2019-01-01'
-close_date='2019-01-12'
-con1=PA4_data_nobrand["created_at"]>=open_date
-con2=PA4_data_nobrand["created_at"]<close_date
-PA4_data_nobrand[con1&con2]
